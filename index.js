0c5a138b8947b954751ce6f64d8c0dd2ac86dee7
Webb Peng <webb.png@gmail.com>
2017-10-08 16:13:28 +0800

    remove object-assign

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const SubLite = require('subscribable-lite');
-const assign = require('object-assign');
 
 const _val = '_subscribable_value';
 const _sub = '_subscribable_lite';
@@ -10,21 +9,23 @@ const SubValue = function SubValue(initialValue) {
 	this[_val] = initialValue;
 };
 
-assign(SubValue.prototype, {
-	get() {
-		return this[_val];
-	},
-	set(nextValue) {
-		const prevValue = this[_val];
-		this[_val] = nextValue;
-		this[_sub].publish(nextValue, prevValue);
-	},
-	subscribe(fn) {
-		return this[_sub].subscribe(fn);
-	},
-	unsubscribe(fn) {
-		return this[_sub].unsubscribe(fn);
-	}
-});
+SubValue.prototype.get = function get() {
+	return this[_val];
+};
+
+SubValue.prototype.set = function set(nextValue) {
+	const prevValue = this[_val];
+	this[_val] = nextValue;
+	this[_sub].publish(nextValue, prevValue);
+	return nextValue;
+};
+
+SubValue.prototype.subscribe = function subscribe(fn) {
+	return this[_sub].subscribe(fn);
+};
+
+SubValue.prototype.unsubscribe = function unsubscribe(fn) {
+	return this[_sub].unsubscribe(fn);
+};
 
 module.exports = SubValue;
